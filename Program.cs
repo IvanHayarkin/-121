111739d657ae078fa58f803bd30e9b5fe305b0a4
Ivan <Khayarkin97@mail.ru>
2022-11-13 21:55:48 +0500

    dz3

--- Program.cs
+++ Program.cs
@@ -16,10 +16,17 @@ if (x > a)
     Console.WriteLine($"Это число самое большое:{x}");
 }
 */
-
+/*
 Console.WriteLine("Введите число: ");
 
 int a = int.Parse(Console.ReadLine()!);
 
 for (int i = 2; i <= a; i += 2)
 Console.WriteLine($"Ответ: {i}");
+*/
+
+Console.WriteLine("Введите число: ");
+
+if (Convert.ToInt32(Console.ReadLine()) % 2==0)
+    Console.WriteLine("Tru");
+else Console.WriteLine("false");
