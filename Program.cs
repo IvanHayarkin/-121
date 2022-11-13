c0874b640df8937f781e6d719f3ae9f5ef451328
Ivan <Khayarkin97@mail.ru>
2022-11-13 21:43:52 +0500

    dz1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите число: ");
+
+int a = int.Parse(Console.ReadLine()!);
+
+int x = int.Parse(Console.ReadLine()!);
+
+if (a > x)
+{
+   
+    Console.WriteLine($"Это число самое большое:{a}");
+}
+if (x > a)
+{
+   
+    Console.WriteLine($"Это число самое большое:{x}");
+}
